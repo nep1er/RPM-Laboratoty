481f833467f75f9710cd972c60a9fe01045525b2
Nepler <151768633+nep1er@users.noreply.github.com>
2026-02-19 23:28:01 +0700

    Lab 2. SOLID

---
+++ Laboratory 2/SolidLab2/Program.cs
@@ -0,0 +1,96 @@
+﻿using SOLID_Fundamentals;
+using System;
+
+namespace SolidLab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var savings = new SavingsAccount();
+            savings.Deposit(1000m);
+            Console.WriteLine($"Создан сберегательный счет. Баланс =  {savings.Balance} руб.");
+
+            var checking = new CheckingAccount();
+            checking.Deposit(2000m);
+            Console.WriteLine($"Создан текущий счет. Баланс = {checking.Balance} руб.");
+
+            var fixedDeposit = new FixedDepositAccount(DateTime.Now.AddMonths(6));
+            fixedDeposit.Deposit(5000m);
+            Console.WriteLine($"Создан срочный депозит. Баланс =  {fixedDeposit.Balance} руб. (доступен через 6 месяцев)");
+
+            
+            Console.WriteLine("\n--- Операции со счетами ---");
+            var bank = new Bank();
+
+            Console.WriteLine("Снимаем 500 руб со сберегательного счета...");
+            bank.ProcessWithdrawal(savings, 500m);
+
+            Console.WriteLine("Переводим 200 руб сберегательный на текущий...");
+            bank.Transfer(savings, checking, 200m);
+
+            Console.WriteLine($"Баланс сберегательного счета: {savings.Balance} руб.");
+            Console.WriteLine($"Баланс текущего счета: {checking.Balance} руб.");
+
+            
+            Console.WriteLine("\n--- ЗАКАЗ ---\n");
+
+            var order = new Order
+            {
+                Id = 1,
+                TotalAmount = 1500m,
+                CustomerEmail = "customer@mail.com",
+                CustomerPhone = "123456789",
+                PaymentMethod = "CreditCard",
+                Items = new List<string> { "Item1", "Item2" }
+            };
+
+            Console.WriteLine($"Создан заказ #{order.Id} на сумму {order.TotalAmount} руб.");
+
+            
+            Console.WriteLine("\n--- Обработка заказа ---");
+
+            var repository = new OrderRepository();
+            var validator = new OrderValidator();
+            var paymentProcessor = new PaymentProcessor();
+            var inventoryService = new InventoryService();
+            var notificationSender = new EmailNotificationSender();
+            var logger = new DatabaseLogger();
+            var receiptGenerator = new ReceiptGenerator();
+
+            var orderProcessor = new OrderProcessor(
+                repository, validator, paymentProcessor,
+                inventoryService, notificationSender,
+                logger, receiptGenerator);
+
+            orderProcessor.AddOrder(order);
+            orderProcessor.ProcessOrder(1);
+
+            
+            Console.WriteLine("\n--- Отчеты ---");
+            var reportGenerator = new ReportGenerator();
+            reportGenerator.GenerateMonthlyReport(repository.GetAll());
+
+            
+            Console.WriteLine("\n--- Уведомления ---");
+
+            var emailService = new SmtpEmailService();
+            var smsService = new TwilioSmsService();
+            var notificationService = new CompositeNotificationService(emailService, smsService);
+
+            var orderService = new OrderService(notificationService);
+            orderService.PlaceOrder(order);
+
+            var promotionService = new PromotionService(notificationService);
+            promotionService.SendPromotion(order.CustomerEmail, "20% discount this weekend!");
+
+
+            Console.WriteLine("\n--- Счет ---");
+            Console.WriteLine($"Текущий счет: {checking.Balance} руб.");
+            Console.WriteLine($"Сберегательный счет: {savings.Balance} руб.");
+            Console.WriteLine($"Срочный депозит: {fixedDeposit.Balance} руб.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Laboratory 2/SolidLab2/Services.cs
@@ -0,0 +1,125 @@
+﻿namespace SOLID_Fundamentals
+{
+    public interface IEmailService
+    {
+        void SendEmail(string to, string subject, string body);
+    }
+
+    public interface ISmsService
+    {
+        void SendSms(string phoneNumber, string message);
+    }
+
+    public interface INotificationService
+    {
+        void SendNotification(string recipient, string message, NotificationType type);
+    }
+
+    public enum NotificationType
+    {
+        Email,
+        Sms,
+        Both
+    }
+
+    public class SmtpEmailService : IEmailService
+    {
+        public void SendEmail(string to, string subject, string body)
+        {
+            Console.WriteLine($"Sending email to {to}: {subject} - {body}");
+        }
+    }
+
+    public class TwilioSmsService : ISmsService
+    {
+        public void SendSms(string phoneNumber, string message)
+        {
+            Console.WriteLine($"Sending SMS to {phoneNumber}: {message}");
+        }
+    }
+
+    public class CompositeNotificationService : INotificationService
+    {
+        private readonly IEmailService _emailService;
+        private readonly ISmsService _smsService;
+
+        public CompositeNotificationService(IEmailService emailService, ISmsService smsService)
+        {
+            _emailService = emailService ?? throw new ArgumentNullException(nameof(emailService));
+            _smsService = smsService ?? throw new ArgumentNullException(nameof(smsService));
+        }
+
+        public void SendNotification(string recipient, string message, NotificationType type)
+        {
+            switch (type)
+            {
+                case NotificationType.Email:
+                    _emailService.SendEmail(recipient, "Notification", message);
+                    break;
+                case NotificationType.Sms:
+                    _smsService.SendSms(recipient, message);
+                    break;
+                case NotificationType.Both:
+                    _emailService.SendEmail(recipient, "Notification", message);
+                    _smsService.SendSms(recipient, message);
+                    break;
+            }
+        }
+    }
+
+    public class OrderService
+    {
+        private readonly INotificationService _notificationService;
+
+        public OrderService(INotificationService notificationService)
+        {
+            _notificationService = notificationService ?? throw new ArgumentNullException(nameof(notificationService));
+        }
+
+        public void PlaceOrder(Order order)
+        {
+            Console.WriteLine($"Placing order {order.Id}...");
+
+            _notificationService.SendNotification(order.CustomerEmail, "Your order has been placed", NotificationType.Email);
+
+            if (!string.IsNullOrEmpty(order.CustomerPhone))
+            {
+                _notificationService.SendNotification(order.CustomerPhone, "Your order has been placed", NotificationType.Sms);
+            }
+        }
+    }
+
+    public class PromotionService
+    {
+        private readonly INotificationService _notificationService;
+
+        public PromotionService(INotificationService notificationService)
+        {
+            _notificationService = notificationService ?? throw new ArgumentNullException(nameof(notificationService));
+        }
+
+        public void SendPromotion(string email, string promotion)
+        {
+            _notificationService.SendNotification(email, promotion, NotificationType.Email);
+        }
+    }
+
+    public static class ServiceFactory
+    {
+        public static OrderService CreateOrderService()
+        {
+            var emailService = new SmtpEmailService();
+            var smsService = new TwilioSmsService();
+            var notificationService = new CompositeNotificationService(emailService, smsService);
+            return new OrderService(notificationService);
+        }
+
+        public static PromotionService CreatePromotionService()
+        {
+            var emailService = new SmtpEmailService();
+            var smsService = new TwilioSmsService();
+            var notificationService = new CompositeNotificationService(emailService, smsService);
+            return new PromotionService(notificationService);
+        }
+    }
+}
