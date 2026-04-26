28c93129d4855e4a282a1adba73d6f090c6b65c2
Azayk <timofejevtifeev7@gmail.com>
2026-04-27 02:52:45 +0700

    Merge PhoneBook

---
+++ PhoneBook/ViewModels/RelayCommand.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace PhoneBook.ViewModels
+{
+    // Команда без параметра для привязки к UI-элементам.
+    public class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool>? _canExecute;
+
+        public RelayCommand(Action execute, Func<bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke() ?? true;
+
+        public void Execute(object? parameter)
+        {
+            if (CanExecute(parameter))
+                _execute();
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+
+    // Команда с параметром (для удаления конкретного элемента).
+    public class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T?> _execute;
+        private readonly Predicate<T?>? _canExecute;
+
+        public RelayCommand(Action<T?> execute, Predicate<T?>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke((T?)parameter) ?? true;
+
+        public void Execute(object? parameter)
+        {
+            if (CanExecute(parameter))
+                _execute((T?)parameter);
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+    }
+}
